d3c3a76b683e0474c0b3dba7f7aa99d10af2242f
icxcc4820 <icxcc4820@windows10.microdone.cn>
2020-10-05 22:23:40 -0700

    the initial version

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;	
+	  
+	 /**
+	   * This method is the constructor of AddingMachine. 
+	   * @param Unused
+	   * @return nothing 
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * This method is used to get toal sum. 
+	   * @param Unused
+	   * @return int 0 
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * This method is used to add a number to total. 
+	   * @param value is the integer to be added to the sum total.
+	   * @return nothing 
+	   */
+	  public void add (int value) {
+	  }
+	  
+	  /**
+	   * This method is used to subtract the number from the total. 
+	   * @param value is the integer to be subtracted from the sum total.
+	   * @return nothing 
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * This method is used to print out the outcome in string type
+	   * @param Unused
+	   * @return a string 
+	   */
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /**
+	   * This method is used to clear and reset the total. 
+	   * @param unused
+	   * @return nothing 
+	   */
+	  public void clear() {
+	  }
+
+}
